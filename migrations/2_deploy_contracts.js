3569367430e021bc43421a75b86a7dd18ccb063a
Clement Lakhal <clementlakhal@gmail.com>
2019-01-11 13:34:11 +0100

    Updated deployment options

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,28 @@
 /* global artifacts */
 
 const COO = artifacts.require('COO');
+const DummyToken = artifacts.require('test/DummyToken');
+const Marketplace = artifacts.require('Marketplace');
 
-function deployContracts(deployer) {
-  deployer.deploy(COO);
+function deployContracts(deployer, network) {
+  console.log('Deploying to network:', network);
+
+  if (network === 'development') {
+    let cooContractAddress;
+    let tokenContractAddress;
+
+    deployer.deploy(COO)
+      .then(() => {
+        cooContractAddress = COO.address;
+
+        return deployer.deploy(DummyToken);
+      })
+      .then(() => {
+        tokenContractAddress = DummyToken.address;
+
+        return deployer.deploy(Marketplace, tokenContractAddress, cooContractAddress);
+      });
+  }
 }
 
 module.exports = deployContracts;
