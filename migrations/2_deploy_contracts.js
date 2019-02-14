c13db65b478e5b26d543c1b7690e900df11c44cf
Clement Lakhal <clementlakhal@gmail.com>
2019-02-14 10:16:17 +0100

    Updated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,13 +2,29 @@
 /* global artifacts */
 
 const COO = artifacts.require('COO');
+const MetaCOO = artifacts.require('MetaCOO');
 const DummyToken = artifacts.require('test/DummyToken');
 const Marketplace = artifacts.require('Marketplace');
 
 function deployContracts(deployer, network) {
   console.log('Deploying to network:', network);
 
-  if (network === 'development' || network === 'ropsten') {
+  if (network === 'development') {
+    let cooContractAddress;
+    let tokenContractAddress;
+
+    deployer.deploy(MetaCOO)
+      .then(() => {
+        cooContractAddress = MetaCOO.address;
+
+        return deployer.deploy(DummyToken);
+      })
+      .then(() => {
+        tokenContractAddress = DummyToken.address;
+
+        return deployer.deploy(Marketplace, tokenContractAddress, cooContractAddress);
+      });
+  } else if (network === 'ropsten') {
     let cooContractAddress;
     let tokenContractAddress;
 
