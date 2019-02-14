4954922e1aee2880356a4cb797a85ade4db53eba
Clement Lakhal <clementlakhal@gmail.com>
2019-02-14 13:29:55 +0100

    Updated certificates array visibility

--- contracts/COO.sol
+++ contracts/COO.sol
@@ -29,7 +29,7 @@ contract COO is ERC721Full, Ownable {
         string data;
     }
 
-    Certificate[] private certificates;
+    Certificate[] internal certificates;
 
     /**
      * @dev Creates a new certificate and links it to the sender
